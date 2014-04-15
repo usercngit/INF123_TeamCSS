fe556338d4e92b8fde9286c227f54c64445ada2d
Chris Noel <cnoel@uci.edu>
2014-04-14 17:09:09 -0700

    Classes to fill
    Also need dot and line objects

---
+++ src_Game/GameBoard.py
@@ -0,0 +1,14 @@
+'''
+Created on Apr 14, 2014
+
+@author: Chris
+'''
+
+#TODO
+    # Size
+    # board objects
+
+class board:
+    
+    def __init__(self, rows, columns):
+        self.rows, self.columns = rows, columns
--- src_Game/main.py
+++ src_Game/main.py
@@ -3,3 +3,6 @@ Created on Apr 11, 2014
 
 @author: Chris
 '''
+
+import GameScreen
+import GameBoard
