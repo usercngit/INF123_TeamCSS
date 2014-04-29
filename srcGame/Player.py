805e7edc4ab8d564ea4767d568cc0d2b66a61367
Sofanah Alrobayan <alrobays@uci.edu>
2014-04-28 17:17:39 -0700

    added player class

---
+++ srcGame/Player.py
@@ -0,0 +1,15 @@
+'''
+@author: Chris
+@author: Sofanah
+@author: Shibani
+'''
+
+class Player:
+	
+	def __init__(self, name, color):
+		self._name = name 
+		self._color = color
+		self._score = 0
+		
+	def score_inc(self):
+		score +=1
