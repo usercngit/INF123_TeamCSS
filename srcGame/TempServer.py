b7e72534a87177bf67184dfe3ed83eeb0a283f02
Shibani Dhume <dhumes@uci.edu>
2014-05-20 22:34:46 -0700

    temp server
    getting error on game object

--- srcGame/TempServer.py
+++ srcGame/TempServer.py
@@ -1,9 +1,51 @@
+from network import Listener, Handler, poll
+from random import randint
+from time import sleep
+from Viewport import FixedViewport
+from Board import Board
+from Player import Player
 
+global VIEWPORT
+VIEWPORT = FixedViewport(900, 600)
+
+global board
+board = Board(VIEWPORT.width, VIEWPORT.height, 6, 6, 5)
+
+
+def broadcast(msg):
+    for h in handlers.keys():
+        h.do_send(msg)
 
 class TempServer:
 
 	def __init__(self):
 		pass
+
+	def on_open(self):
+		handlers[self] = None
+		print "connected to server"
+
+	def on_close(self):
+		name = handlers[self]
+        del handlers[self]
+        print "disconnected from server"
+
+	def on_msg(self):
+		if 'join' in msg:
+			if board.add_player(handlers[self]):
+				board.add_player(handlers[self])
+				name = msg['join']
+				handlers[self] = name
+				broadcast({'join': name, 'users': handlers.values()})
+			else:
+				pass
+		elif 'quit' in msg:
+			del handlers[self]
+			broadcast({'leave': name})
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			board.update(event.pos)
+
+
 
 '''
 on_open():
